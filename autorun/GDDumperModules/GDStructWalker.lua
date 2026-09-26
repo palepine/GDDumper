63e4b506106c836a0be4790a55b8bd4eb6953374
palepine <paledpine@gmail.com>
2026-09-26 15:06:31 +0300

    relicense

--- autorun/GDDumperModules/GDStructWalker.lua
+++ autorun/GDDumperModules/GDStructWalker.lua
@@ -1,3 +1,20 @@
+--[[
+  Cheat Engine Godot Dumper — Copyright (C) 2026 palepine
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]
+    
 local Module = {}
 -- That's a debug module which's optional and used only to guess offsets to be recorded in the hardoffset module, at least for stable engine versions
 -- obviously enough, for this to work, the root 
--- autorun/GDumper.lua
+++ autorun/GDumper.lua
@@ -1,6 +1,23 @@
--- This script was created by palepine. Support me: https://www.patreon.com/c/palepine/membership
--- I'd like to thank cfemen for some basic insights about the godot engine which saved me from reading much of the Godot Engine source code initially.
--- Source code on github: https://github.com/palepine/GDDumper
+--[[
+  Cheat Engine Godot Dumper — Copyright (C) 2026 palepine
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]
+
+
+-- support me: https://www.patreon.com/c/palepine/membership
+
 -- ///---///--///---///--///---///--///--///---///--///---///--///---///--/// TODOS
   -- TODO tree view form
   -- TODO doxygen comments
